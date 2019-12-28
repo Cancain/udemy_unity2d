de11cffd9979ac523772beaf1513cb028819f51b
Cancain <eriksson.tomas@gmail.com>
2019-12-28 18:42:41 +0100

    completed lecture 38

---
+++ number_wizard/Assets/Scripts/Scene_Loader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Loader : MonoBehaviour {
+
+    public void LoadNextScene() {
+        int currentSeenIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSeenIndex + 1);
+    }
+}
