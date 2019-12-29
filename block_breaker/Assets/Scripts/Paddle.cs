067b4f9e52e105dd3305e1d465dc384100aeaf29
Cancain <eriksson.tomas@gmail.com>
2019-12-29 19:45:18 +0100

    completed lecture 53

---
+++ block_breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    private void Update() {
+        float mouseXPosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(mouseXPosInUnits, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
