808ea9f69dcec935defda81469a0ff3ef3a50693
Cancain <eriksson.tomas@gmail.com>
2019-12-29 19:59:55 +0100

    completed lecture 54

--- block_breaker/Assets/Scripts/Paddle.cs
+++ block_breaker/Assets/Scripts/Paddle.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
 
+    //Config parameters
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float minX = 1f;
     [SerializeField] float screenWidthInUnits = 16f;
     private void Update() {
         float mouseXPosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(mouseXPosInUnits, transform.position.y);
+        paddlePos.x = Mathf.Clamp(paddlePos.x, minX, maxX);
         transform.position = paddlePos;
     }
 }
